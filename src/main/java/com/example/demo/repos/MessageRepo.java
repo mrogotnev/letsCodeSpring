86f4e7f5a27141232d301071ebff1aeb4f6ba1dc
mrogotnev@yandex.ru <f3FHDari>
2022-09-12 21:39:25 +0400

    second lesson done

--- src/main/java/com/example/demo/repos/MessageRepo.java
+++ src/main/java/com/example/demo/repos/MessageRepo.java
@@ -3,6 +3,8 @@ package com.example.demo.repos;
 import com.example.demo.domain.Message;
 import org.springframework.data.repository.CrudRepository;
 
-public interface MessageRepo extends CrudRepository<Message, Long> {
+import java.util.List;
 
+public interface MessageRepo extends CrudRepository<Message, Long> {
+    List<Message> findByTag(String tag);
 }
